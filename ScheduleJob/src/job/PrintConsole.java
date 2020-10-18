3e3a23d84c16f8fc1673b83c8886e09fd1008478
Manivannan R <sendmsg2mani@gmail.com>
2020-10-18 11:43:22 +0530

    Update PrintConsole.java

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -3,10 +3,8 @@ package job;
 public class PrintConsole {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Hurray, the first Jenkins Job");
-		
-
+		System.out.println("Hurray, the first update to Jenkins Job");
 	}
 
 }
